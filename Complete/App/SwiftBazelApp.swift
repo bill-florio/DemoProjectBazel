ccbc5cdcc2a172b0d97728fef65ec4822dee786d
Bill Chan <billypchan@gmail.com>
2022-03-01 23:02:06 +0100

    testing SwiftUI code

---
+++ Complete/App/SwiftBazelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBazelApp.swift
+//  SwiftBazel
+//
+//  Created by Yiu Chan on 01.03.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBazelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
